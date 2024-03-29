205cf7b3861518b7f8709496e39f48675d0f6f58
Valued Customer <Valued Customer@DUM-LAPTOP>
2024-03-29 16:29:17 -0600

    console.writeline changed

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Connect_four
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("A connect four game");
+        }
+    }
+}
